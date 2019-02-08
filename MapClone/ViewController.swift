123dd3934f79fb9aa02ecb889d10f8b633fa57d9
Tieda Wei <weitieda@gmail.com>
2019-02-07 22:59:22 -0500

    Initial Commit

---
+++ MapClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapClone
+//
+//  Created by Tieda Wei on 2019-02-07.
+//  Copyright © 2019 Tieda Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
