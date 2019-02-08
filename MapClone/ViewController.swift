d8ba669d57302bf61efec75a9a965fbf4809e591
Tieda Wei <weitieda@gmail.com>
2019-02-07 23:01:52 -0500

    Initial commit

--- MapClone/ViewController.swift
+++ MapClone/ViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        view.backgroundColor = .red
     }
-
-
 }
 
